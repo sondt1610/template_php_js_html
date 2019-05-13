95730fa12fab4ac53bae394142b2b9e8ecb070ac
sondt6951 <sondt6951@co-well.com.vn>
2019-05-13 15:55:34 +0700

    fdfd

---
+++ main.js
@@ -0,0 +1,7 @@
+var Message = React.createClass({
+        render:function(){
+            return <h1>Welcome to TutsPlus</h1>
+        }
+    });
+ 
+    ReactDOM.render(<Message />,document.getElementById('main'));
